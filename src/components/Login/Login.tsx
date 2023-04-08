1c4b91a218d3073127fb59e03b21f94cdcd758bb
geogeorge1997 <igeogeorge1997@gmail.com>
2023-04-08 11:51:34 +0530

    useEffect changes

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { setLogin } from '../../redux/index'
